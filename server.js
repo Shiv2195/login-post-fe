e1d5b7404a5125f65251be7d7058a5d7c07a769c
Shiv2195 <35445257+Shiv2195@users.noreply.github.com>
2019-12-18 00:54:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/jalan-task-fe'));
 
-app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/jalan-task-fe/src/index.html'));
-});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
