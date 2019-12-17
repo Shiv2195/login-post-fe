4593a9cedfdb8d88e3407c4468ee55e03165f8e6
dope <shiv2195@gmail.com>
2019-12-18 01:37:20 +0530

    deployment changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/jalan-task-fe/'));
+app.use(express.static(__dirname + '/dist/jalan-task-fe'));
 
 app.get('/*', function(req,res) {
 
