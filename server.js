ce8be6290e18ddea93628f4cb656d52dd1d71711
dope <shiv2195@gmail.com>
2019-12-18 01:50:11 +0530

    deployment changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/jalan-task-fe'));
+app.use(express.static(__dirname + '/dist/jalan-fe'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/jalan-task-fe/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/jalan-fe/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
