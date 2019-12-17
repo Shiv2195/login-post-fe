ea778bf553f05a818957806af3c368dcd4303ab3
Shiv2195 <35445257+Shiv2195@users.noreply.github.com>
2019-12-18 00:48:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/jalan-task-fe'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/jalan-task-fe/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/jalan-task-fe/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
